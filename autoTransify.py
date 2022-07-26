e5544e93763c0db1331b9db8597ddbf6873b5466
neil.zhu <neil.zhu@shopee.com>
2022-07-26 14:13:18 +0800

    open browser once. Edit comments

--- autoTransify.py
+++ autoTransify.py
@@ -2,7 +2,6 @@
 # sample call: python autoTransify.py 97 C:/Users/neil.zhu/1.xlsx
 # 脚本 + transify project code + 翻译文件路径
 # need to edit the user-data-dir 
-# need to install python + selenium + webdriver_manager + chrome driver + pandas + openpyxl
 
 import os
  
@@ -20,21 +19,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-# from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-
 import pandas as pd
 import sys
 
-
-def operationFill(url,key,translation):
-    option = webdriver.EdgeOptions()
-    option.add_argument("user-data-dir=C:/Users/neil.zhu/AppData/Local/Microsoft/Edge/User Data")    # 浏览器路径,保存浏览器缓存登录transify
-    option.add_experimental_option('excludeSwitches', ['enable-logging'])
-    # option.add_experimental_option("detach",True)
-    driver = webdriver.Edge(EdgeChromiumDriverManager().install(),options=option)  
+# 验证现有transify，如果与excel不同，则填充
+def operationFill(driver,url,key,translation):
     driver.get(url)
-    # time.sleep(8)
     driver.implicitly_wait(10)
     elemsearch = driver.find_element(By.CLASS_NAME,'input')
     elemsearch.send_keys(key)
@@ -49,9 +40,7 @@ def operationFill(url,key,translation):
     # elementsave = driver.find_element(By.CLASS_NAME,'large')
     # elementsave.click()
     # time.sleep(3)
-
     result = elementkey.get_attribute('value')
-
     while result != translation:
         elementkey.click()
         time.sleep(1)
@@ -61,21 +50,29 @@ def operationFill(url,key,translation):
         time.sleep(1)
         elementsave = driver.find_element(By.CLASS_NAME,'large')
         elementsave.click()
-        time.sleep(3)
+        time.sleep(2)
         result = elementkey.get_attribute('value')
-
     print('已成功更新 '+ key +' '+ result)
 
+
+# 读取本地excel
 def read_excel_list(path_file_name):
     excel_data = pd.read_excel(path_file_name,engine='openpyxl')
     df_list = excel_data.values.tolist()
     return df_list
 
+
+# 主方法，读取excel，填充transify
 def main(url,path):
+    option = webdriver.EdgeOptions()
+    option.add_argument("user-data-dir=C:/Users/neil.zhu/AppData/Local/Microsoft/Edge/User Data")    # 浏览器路径,保存浏览器缓存登录transify
+    option.add_experimental_option('excludeSwitches', ['enable-logging'])
+    # option.add_experimental_option("detach",True)
+    driver = webdriver.Edge(EdgeChromiumDriverManager().install(),options=option)  
     a = read_excel_list(path)
     count = 1
     for i in a: 
-       operationFill(url,i[0],i[1])
+       operationFill(driver,url,i[0],i[1])
        print('已成功更新',str(count),'个翻译')
        count=count+1
 
@@ -90,8 +87,4 @@ if __name__ == '__main__':
     file_path = str(sys.argv[2])  # transify路径，格式 key+翻译
     main(url,file_path)
 
-
-# url = "https://transify.sea.com/resources/360/lang/34"
-# a = read_excel_list('C:/Users/neil.zhu/test.xlsx')
-# for i in a:
-#     operationFill(url,i[0],i[1])
+)
